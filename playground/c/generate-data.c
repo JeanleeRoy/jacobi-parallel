2bc319f78fc8bc183db2f531bc9d0580181878bd
Jeanlee Barreto <jeanlee-roy@hotmail.com>
2023-11-30 01:09:31 -0500

    chore: update files

--- playground/c/generate-data.c
+++ playground/c/generate-data.c
@@ -2,24 +2,41 @@
 
 #include "../../lib/utils.h"
 
+// Compile: gcc -o generate-data.out generate-data.c -lm
+
 #define N 100
 
 int main(int argc, char const *argv[]) {
     int size = N;  // Size of the system of equations
+    int is_fortran = 0;
 
-    if (argc == 2) {
+    if (argc >= 2) {
         size = atoi(argv[1]);
+    }
+    if (argc == 3) {
+        is_fortran = !!atoi(argv[2]);
     }
 
     double *A = (double *)malloc(size * size * sizeof(double));
     double *b = (double *)malloc(size * sizeof(double));
-    
+
     generate_diagonal_dominant_matrix(A, size, 0, 1200);
     generate_vector(b, size, 10, 200);
 
+    if (is_fortran) printf("\nC format:\n");
     printf("%d\n", size);
-    print_array_inline(A, size * size);
-    print_array_inline(b, size);
-    
+    print_array_inline(A, size * size, 0);
+    print_vector_inline(b, size);
+
+    if (is_fortran) {
+        printf("\nFotran format:\n");
+        printf("%d\n", size);
+        print_array_inline(A, size * size, 1);
+        print_vector_inline(b, size);
+    }
+
+    free(A);
+    free(b);
+
     return 0;
 }
